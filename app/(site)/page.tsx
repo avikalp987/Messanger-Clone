57d9cbb11e0d505668c51daf100aab27ac674666
vikalp <avikalp233@gmail.com>
2023-12-20 18:19:51 +0530

    Auth setup

---
+++ app/(site)/page.tsx
@@ -0,0 +1,24 @@
+import Image from "next/image";
+
+export default function Home() {
+    return (
+        <div className="flex flex-col min-h-full justify-center py-12 sm:px-6 lg:px-8 bg-gray-100">
+           <div className="sm:mx-auto sm:w-full sm:max-w-md">
+                <Image 
+                    alt="logo"
+                    height="48"
+                    width="48"
+                    src={"/Images/logo.png"}
+                    className="mx-auto w-auto"
+                />
+
+                <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
+                    Sign in to your account
+                </h2>
+           </div>
+
+           {/** Auth Form */}
+        </div>
+    )
+
+}
