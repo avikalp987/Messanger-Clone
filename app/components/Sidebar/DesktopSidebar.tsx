7594564b0d7c4fc7274a5bfdfe5ed158f19a12e5
vikalp <avikalp233@gmail.com>
2023-12-24 20:03:42 +0530

    Settings modal and functionality

--- app/components/Sidebar/DesktopSidebar.tsx
+++ app/components/Sidebar/DesktopSidebar.tsx
@@ -5,6 +5,7 @@ import useRoutes from "../../hooks/useRoutes"
 import DesktopItem from "./DesktopItem"
 import { User } from "@prisma/client"
 import Avatar from "../Avatar"
+import SettingsModal from "./SettingsModal"
 
 interface DesktopSidebarProps {
     currentUser: User
@@ -18,6 +19,12 @@ const DesktopSidebar: React.FC<DesktopSidebarProps> = ({
     const [isOpen, setIsOpen] = useState<boolean>(false);
 
     return (
+        <>
+        <SettingsModal 
+            currentUser={currentUser}
+            isOpen={isOpen}
+            onClose={()=>setIsOpen(false)}
+        />
         <div className="hidden lg:fixed lg:inset-y-0 lg:left-0 lg:z-40 lg:w-20 xl:px-6 lg:overflow-y-auto lg:bg-white lg:border-r-[1px] lg:pb-4 lg:flex lg:flex-col justify-between">
             <nav className="mt-4 flex flex-col justify-between">
                 <ul 
@@ -48,6 +55,7 @@ const DesktopSidebar: React.FC<DesktopSidebarProps> = ({
                 </div>
             </nav>
         </div>
+        </>
     )
 }
 
