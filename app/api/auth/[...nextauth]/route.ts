42d1ed9f7c49b0f66ec0680623e7047f1f69503b
vikalp <avikalp233@gmail.com>
2023-12-26 11:09:31 +0530

    fix-(c)

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,4 +1,4 @@
-import { authOptions } from "@/app/libs/authOptions";
+import { authOptions } from "../../../libs/authOptions";
 import NextAuth from "next-auth"
 
 const handler = NextAuth(authOptions);
