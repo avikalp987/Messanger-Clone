1dffe517986e39f5effd39956c826a61c6ae74d8
vikalp <avikalp233@gmail.com>
2023-12-26 11:12:51 +0530

    fix-(d)

--- pages/api/pusher/auth.ts
+++ pages/api/pusher/auth.ts
@@ -1,4 +1,5 @@
-import { authOptions } from "../../../app/api/auth/[...nextauth]/route";
+
+import { authOptions } from "../../../app/libs/authOptions";
 import { pusherServer } from "../../../app/libs/pusher";
 import { NextApiRequest, NextApiResponse } from "next";
 import { getServerSession } from "next-auth";
